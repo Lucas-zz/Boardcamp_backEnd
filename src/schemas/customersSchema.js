1482462ae8db8f74f2a3559b893b06180dff5de8
Lucas Azzolini Vieira <lucasazzollinivieira@gmail.com>
2022-03-04 14:35:50 -0300

    Update customersSchema.js

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -4,7 +4,7 @@ const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().pattern(/^[0-9]{10,11}$/).required(),
     cpf: joi.string().pattern(/^[0-9]{11}$/).required(),
-    birthday: joi.string().pattern(/^([0-9]{4}[-]?((0[13-9]|1[012])[-]?(0[1-9]|[12][0-9]|30)|(0[13578]|1[02])[-]?31|02[-]?(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)[-]?02[-]?29)$/).required(),
+    birthday: joi.string().pattern(/^\d{4}\-\d{2}\-\d{2}$/).required(),
 });
 
 export default customerSchema;
